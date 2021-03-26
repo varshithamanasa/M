5ddf9ff9824a13bc60e379a56b4b9fc401709dcf
VARSHITHA M <45088568+varshithamanasa@users.noreply.github.com>
2021-03-26 09:15:34 +0530

    Add files via upload
    Assignment 2

---
+++ assignment2/script.js
@@ -0,0 +1,14 @@
+var first;
+var last;
+var email;
+var age;
+function validate()
+{
+    first = document.getElementById("fname").value;
+    last = document.getElementById("lname").value;
+    email = document.getElementById("email").value;
+    age = document.getElementById("age").value;
+    
+    document.write('<table>')
+
+}
